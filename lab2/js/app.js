608aea5b80a9a7470f296b12e1a1f59da76f8f22
Marcus Heine <mheine@kth.se>
2016-01-29 16:31:20 +0100

    Add some sort of previousView

--- lab2/js/app.js
+++ lab2/js/app.js
@@ -5,4 +5,6 @@ $(function() {
 	//And create the needed controllers and views
 	var exampleView = new ExampleView($("#exampleView"));
 
+	var previousView = new PreviousView($("#previousparties"));
+
 });
