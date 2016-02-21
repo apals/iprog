51b1d1d726b3b00cb663d7ec83b85393d89b2928
Marcus Heine <mheine@kth.se>
2016-02-21 17:41:06 +0100

    Updates header info

--- lab3/js/view/headerInfoView.js
+++ lab3/js/view/headerInfoView.js
@@ -2,14 +2,19 @@
  * Created by apals on 21/02/16.
  */
 
-/*
-
- <h2 class="left">My Dinner: 2 people</h2>
- <button class="right">Go back and edit dinner</button>
- */
-
 //ExampleView Object constructor
 var HeaderInfoView = function (container, model) {
-    container.append($('<h2>').addClass("left").append(model.getPartyName() + ": " + model.getNumberOfGuests() + " people"));
-    container.append($('<button>').addClass("right").append("Go back and edit dinner"));
+
+	model.addObserver(this);
+	setHtml();
+
+	function setHtml() {
+		container.append($('<h2>').addClass("left").append(model.getPartyName() + ": " + model.getNumberOfGuests() + " people"));
+    	container.append($('<button>').addClass("right").append("Go back and edit dinner"))
+	}
+    
+    this.update function () {
+    	container.empty();
+    	setHtml();
+    }
 };
