c437d94a3e28c9104ab81617fee514907fe57c06
Marcus Heine <mheine@kth.se>
2016-02-21 14:48:18 +0100

    Remove unnecessary files

--- lab3/js/app.js
+++ lab3/js/app.js
@@ -4,15 +4,13 @@ $(function () {
     model.generateTestParty();
 
     //And create the needed controllers and views
-    //  var exampleView = new ExampleView($("#exampleView"));
-    var newPartyView = new NewPartyView($("#newPartyView"));
+
     var ingredientsView = new IngredientsView($("#ingredientsView"), model);
     var preparationsView = new PreparationsView($("#preparationsView"), model);
 
     var overviewView = new OverviewView($("#overviewView"), model);
     var selectDishView = new SelectDishView($("#selectDishView"), model);
     var panelView = new PanelView($("#panel"), model);
-    var previousView = new PreviousView($("#previousparties"));
     var mealsView = new MealsView($("#meals-view"), model);
 
     var detailView = new DetailView($("#detail-view"), model);
