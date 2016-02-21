66692a901e2f2477e91910816015f421e7d29727
Andreas Pålsson <apals@kth.se>
2016-02-21 17:28:56 +0100

    the things

--- lab3/js/view/panelView.js
+++ lab3/js/view/panelView.js
@@ -12,7 +12,7 @@ var PanelView = function (container, model) {
     container.append($("<hr>"));
     container.append($('<input>').attr("type", "text").attr("id", "name-of-party-input").attr("placeholder", "Party name"));
     container.append($("<hr>"));
-    container.append($('<input>').attr("type", "number").attr("id", "number-of-guests-input").attr("placeholder", "Guests").attr("value", "0"));
+    container.append($('<input>').attr("type", "number").attr("id", "number-of-guests-input").attr("placeholder", "Guests").attr("value", "0").attr("min", "1"));
     container.append($("<hr>"));
     container.append($("<h4>").append("Dish Name - Cost"));
     container.append($("<div>").attr("id", "dish-and-cost"));
@@ -29,7 +29,7 @@ var PanelView = function (container, model) {
 
 
         for (var i = menu.length - 1; i >= 0; i--) {
-            $("#dish-and-cost").append(menu[i].name + " - " + model.getDishPrice(menu[i].id) + " kr").append("<br>");
+            $("#dish-and-cost").append(menu[i].name + " - " + model.getDishPrice(menu[i].id) * model.getNumberOfGuests() + " kr").append("<br>");
         }
 
         $("#dish-and-cost").append("<br><br>Total menu cost: " + model.getTotalMenuPrice() * model.getNumberOfGuests() + "kr.");
