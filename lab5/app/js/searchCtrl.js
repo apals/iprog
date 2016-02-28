8446a713ed416b658a209dd60055725384e25f6b
Andreas Pålsson <apals@kth.se>
2016-02-28 13:11:55 +0100

    Add get for recipe/:id and display instructions

--- lab5/app/js/searchCtrl.js
+++ lab5/app/js/searchCtrl.js
@@ -9,6 +9,7 @@ dinnerPlannerApp.controller('SearchCtrl', function ($scope, Dinner) {
         $scope.status = "Searching...";
         Dinner.DishSearch.get({title_kw: query}, function (data) {
             $scope.dishes = data.Results;
+            console.log(data.Results);
             $scope.status = "Showing " + data.Results.length + " results";
         }, function (data) {
             $scope.status = "There was an error";
